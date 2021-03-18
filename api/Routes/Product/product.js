3fe98164509d5a7eccb00e0718aa21ce30bf20e4
santiago <santiago.ottolini97@gmail.com>
2021-03-18 09:33:29 -0300

    BE: Get by id

--- api/Routes/Product/product.js
+++ api/Routes/Product/product.js
@@ -2,14 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../../Models/product");
 
-router.get("/", async(req, res)=>{
+router.get("/", async (req, res) => {
   try {
-    const product = await Product.find()
-    res.json(product)
+    const product = await Product.find();
+    res.json(product);
   } catch (error) {
-    res.json({message:error})
+    res.json({ message: error });
   }
-})
+});
+
+router.get("/:productId", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+    res.json(product);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
 
 router.post("/", async (req, res) => {
   const product = new Product({
