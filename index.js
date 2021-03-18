cf8ad928a09dd442664223f87f496435558436e3
santiago <santiago.ottolini97@gmail.com>
2021-03-18 10:30:06 -0300

    BE: Install cors

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const productsRoutes = require("./api/Routes/Product/product");
 const bodyParser = require("body-parser");
-require("dotenv/config")
+const cors = require("cors");
+require("dotenv/config");
+
+//CALL ROUTES
+const productsRoutes = require("./api/Routes/Product/product");
 
 const app = express();
-app.use(bodyParser.json())
 
+//CORS
+app.use(cors());
+
+app.use(bodyParser.json());
+
+//MONGO CONNECT
 mongoose.connect(
-process.env.DB_CONNECTION,  {
+  process.env.DB_CONNECTION,
+  {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   },
   (req, res) => {
-    console.log("connect to the database");
+    console.log("Connect to the database");
   }
 );
 
+//ROUTES
 app.use("/api/product/", productsRoutes);
-
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
